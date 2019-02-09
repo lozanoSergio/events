ffe1d6514711a4cd5e1256ea248e994dd9a999cd
Sergio <sergylozanodelrio@gmail.com>
2019-02-09 13:05:59 +0100

    CRUD static app finish

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,40 +1,62 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
+const emptyEvent = {
+  title: "",
+  date: "",
+  city: "",
+  venue: "",
+  hostedBy: ""
+};
+
 class EventForm extends Component {
   state = {
-      event: {
-          title: '',
-          date: '',
-          city: '',
-          venue: '',
-          hostedBy: ''
-      }
+    event: emptyEvent
+  };
+
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        event: this.props.selectedEvent
+      })
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.selectedEvent !== this.props.selectedEvent) {
+      this.setState({
+        event: nextProps.selectedEvent || emptyEvent
+      })
+    }
   }
 
   onFormSubmit = evt => {
     evt.preventDefault();
-    this.props.createEvent(this.state.event);
+    if (this.state.event.id) {
+      this.props.updateEvent(this.state.event);
+    } else {
+      this.props.createEvent(this.state.event);
+    }
   };
 
-  onInputChange = (evt) => {
-      const newEvent = this.state.event;
-      newEvent[evt.target.name] = evt.target.value
-      this.setState({
-          event: newEvent
-      })
-  }
+  onInputChange = evt => {
+    const newEvent = this.state.event;
+    newEvent[evt.target.name] = evt.target.value;
+    this.setState({
+      event: newEvent
+    });
+  };
 
   render() {
     const { handleCancel } = this.props;
-    const {event} = this.state;
+    const { event } = this.state;
     return (
       <Segment>
         <Form onSubmit={this.onFormSubmit}>
           <Form.Field>
             <label>Event Title</label>
             <input
-              name='title'
+              name="title"
               onChange={this.onInputChange}
               value={event.title}
               placeholder="Event Title"
