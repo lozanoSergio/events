f9a9f4cb00d3fc6c799c918a73e005562f1ba85a
Sergio <sergylozanodelrio@gmail.com>
2019-02-13 13:08:43 +0100

    Added Toastr and bug solutions

---
+++ src/app/data/mockApi.js
@@ -0,0 +1,11 @@
+import sampleDate from './sampleData'
+
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export const fetchSampleData = () => {
+    return delay(1000).then(() => {
+        return Promise.resolve(sampleDate);
+    })
+}
