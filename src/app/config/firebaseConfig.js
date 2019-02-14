9ed2c925b43f23844c45538620936e657130138f
Sergio <36728951+lozanoSergio@users.noreply.github.com>
2019-02-14 20:12:24 +0100

    Update firebaseConfig.js

--- src/app/config/firebaseConfig.js
+++ src/app/config/firebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDG0ZZ8-kp3uyr8t42o7vhEH_R6s51LoQ",
-    authDomain: "lccards-79b16.firebaseapp.com",
-    databaseURL: "https://lccards-79b16.firebaseio.com",
-    projectId: "lccards-79b16",
-    storageBucket: "lccards-79b16.appspot.com",
-    messagingSenderId: "742925869202"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 }
 
 firebase.initializeApp(firebaseConfig);
